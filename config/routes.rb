0e54f8490a96c6212a72b239f6bc4832a6fb0c53
Hassan <hassanhammad2040@gmail.com>
2016-12-28 16:18:13 +0500

    Made tests page with task panels

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
         put :change
       end
     end
-  
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
    root 'pages#home'
 
    get 'about' => "pages#about"
+   get 'test' => "pages#test"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
